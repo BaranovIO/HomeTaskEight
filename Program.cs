021a28047a91ba3d75cc45bb641e96831746a7b9
Igor Baranov <eltery@mail.ru>
2022-12-26 00:59:55 +0300

    Попытка решения второй задачи одним методом

--- Program.cs
+++ Program.cs
@@ -66,7 +66,7 @@ Show2dArray(myArray);
 
 //Задача 56. Задайте прямоугольный двумерный массив. 
 //Напишите программу, которая будет находить строку с наименьшей суммой элементов.
-
+/*
 int[,] CreateRandom2dArray()
 {
     Console.Write("Input a number of rows: ");
@@ -138,5 +138,62 @@ void RowOfMinimumSum(int[] array)
 int[,] myArray = CreateRandom2dArray();
 Show2dArray(myArray);
 RowOfMinimumSum(SumOfRows(myArray));
+*/
 
+int[,] CreateRandom2dArray()
+{
+    Console.Write("Input a number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
 
+    int[,] array = new int[rows, columns];
+
+    for(int i = 0; i < rows; i++)
+    
+        for(int j = 0; j < columns; j++)
+        
+            array[i,j] = new Random().Next(minValue, maxValue +1);
+        
+    
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + "\t");  
+
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
+
+void RowOfMinimumSum(int[,] array)
+{   
+    int iMin = 0, minSumOfRow = 0;
+    
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        int sumOfRow = 0;
+        for(int j = 0; j < array.GetLength(1); j++)
+        
+            sumOfRow = sumOfRow + array[i,j];
+        if(sumOfRow < minSumOfRow)
+        {   minSumOfRow = sumOfRow;
+            iMin = i;
+        }
+    }
+    Console.WriteLine($"The row with minimum sum of elements is {iMin + 1}");
+}
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+RowOfMinimumSum(myArray);
